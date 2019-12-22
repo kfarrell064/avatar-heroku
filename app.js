0e5faed05b2c4fa5f6a859246242afc299676469
Katie Farrell <katiefarrell@Katies-MBP.home>
2019-12-21 23:23:53 -0500

    changed to graphic design

---
+++ app.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("click", function() {console.log("clicked");})
+var request = new XMLHttpRequest();
+request.open("GET", "test.txt", true);
+request.send(null);
+request.onreadystatechange = function() {
+  if (request.readyState == 4)
+    alert(request.responseText);
+};
